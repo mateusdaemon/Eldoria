ad302d32d744b587fb6c05c63dacaf0984f6e7f4
mateusdaemon <mateusjdm@gmail.com>
2024-03-14 12:29:48 -0300

    Atualizando a HUD

--- Eldoria/Assets/Scripts/SpellbookMng.cs
+++ Eldoria/Assets/Scripts/SpellbookMng.cs
@@ -21,30 +21,30 @@ public class SpellbookMng : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
+            currSpellbook = Spellbook.None;
+            greenActive.SetActive(false);
+            redActive.SetActive(false);
+            blueActive.SetActive(false);
+        } else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
             currSpellbook = Spellbook.Green;
             greenActive.SetActive(true);
             redActive.SetActive(false);
             blueActive.SetActive(false);
         }
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             currSpellbook = Spellbook.Red;
             redActive.SetActive(true);
             greenActive.SetActive(false);
             blueActive.SetActive(false);
         }
-        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
         {
             currSpellbook = Spellbook.Blue;
             blueActive.SetActive(true);
             redActive.SetActive(false);
             greenActive.SetActive(false);
-        } else if (Input.GetKeyDown(KeyCode.Space))
-        {
-            currSpellbook = Spellbook.None;
-            greenActive.SetActive(false);
-            redActive.SetActive(false);
-            blueActive.SetActive(false);
         }
     }
 }
