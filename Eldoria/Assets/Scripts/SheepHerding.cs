d09e721c337f51e16e170b3a1d1221ea8c99b512
mateusdaemon <mateusjdm@gmail.com>
2024-03-17 17:59:44 -0300

    IA das ovelhas

---
+++ Eldoria/Assets/Scripts/SheepHerding.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepHerding : MonoBehaviour
+{
+    private GameObject parent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        parent = transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            float dist = Vector3.Distance(this.transform.position, other.gameObject.transform.position);
+            float posX = transform.position.x;
+            float posZ = transform.position.z;
+
+            if (other.gameObject.transform.position.x > posX)
+            {
+                posX -= dist;
+            } else
+            {
+                posX += dist;
+            }
+
+            if (other.gameObject.transform.position.z > posX)
+            {
+                posZ -= dist;
+            }
+            else
+            {
+                posZ += dist;
+            }
+
+            Vector3 target = new Vector3(posX, 0, posZ);
+            parent.transform.position = Vector3.MoveTowards(transform.position, target, 0.5f);
+        }
+    }
+}
