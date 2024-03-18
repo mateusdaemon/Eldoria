7a4e192e0495ae06a5b6bf77bfdb153e934dd782
mateusdaemon <mateusjdm@gmail.com>
2024-03-18 15:29:09 -0300

    Adicionando animação pra ovelha entrando no cercado

--- Eldoria/Assets/Scripts/SheepGoInside.cs
+++ Eldoria/Assets/Scripts/SheepGoInside.cs
@@ -11,17 +11,24 @@ public class SheepGoInside : MonoBehaviour
     private Vector3 pos1, pos2, fitPos;
     private float xBoundArea, zBoundArea;
     private bool goPos1 = false, goPos2 = false, fitArea = false;
+    private Animator anim;
+    private SpriteRenderer sr;
 
     // Start is called before the first frame update
     void Start()
     {
         pos1 = point1.transform.position;
         pos2 = point2.transform.position;
+        
         xBoundArea = area.GetComponent<Collider>().bounds.size.x / 2;
         zBoundArea = area.GetComponent<Collider>().bounds.size.z / 2;
+        
         fitPos = area.transform.position;
         fitPos.x += Random.Range(-xBoundArea, xBoundArea);
         fitPos.z += Random.Range(-zBoundArea, zBoundArea);
+
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -30,6 +37,9 @@ public class SheepGoInside : MonoBehaviour
         if (transform.position != pos1 && goPos1 == true)
         {
             pos1.y = transform.position.y;
+            anim.SetBool("frontWalk", false);
+            anim.SetBool("backWalk", true);
+            anim.SetBool("sideWalk", false);
             transform.position = Vector3.MoveTowards(transform.position, pos1, 0.1f);
         } else if (goPos1)
         {
@@ -40,6 +50,10 @@ public class SheepGoInside : MonoBehaviour
         if (transform.position != pos2 && goPos2 == true)
         {
             pos2.y = transform.position.y;
+            anim.SetBool("frontWalk", false);
+            anim.SetBool("backWalk", false);
+            anim.SetBool("sideWalk", true);
+            sr.flipX = true;
             transform.position = Vector3.MoveTowards(transform.position, pos2, 0.1f);
         }
         else if (goPos2)
