77cab7831266796bb702527a0bbec5621c21ed76
RafaelSFiamengue <rafinhasf2002@gmail.com>
2024-03-25 17:13:33 -0300

    Sound Manager adicionado

---
+++ Eldoria/Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [Header("--- Audio Source ---")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource sfxSource;
+
+    [Header("--- Music ---")]
+    public AudioClip cityBackground;
+    public AudioClip farmBackground;
+    public AudioClip fightBackground;
+
+    [Header("--- PlayerSFX ---")]
+    public AudioClip sfxShooter;
+    public AudioClip sfxShield;
+    public AudioClip sfxBreakCurse;
+    public AudioClip sfxErroBreakCurse;
+    public AudioClip sfxErroManaHeal;
+    public AudioClip sfxDrinkLife;
+    public AudioClip sfxDrinkMana;
+
+    [Header("--- Ambient ---")]
+    public AudioClip windMillSound;
+    public AudioClip chimneySound;
+    public AudioClip sheepSound;
+    public AudioClip birdSound;
+    public AudioClip airSound;
+
+    [Header("--- Effects ---")]
+    public AudioClip wolfBarkSound;
+    public AudioClip sfxWolfDie;
+
+    public void PlaySfx(AudioClip clip)
+    {
+        sfxSource.PlayOneShot(clip);
+    }
+}
