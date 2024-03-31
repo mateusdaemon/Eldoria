eb45892fac2adc44590372702091489797438318
mateusdaemon <mateusjdm@gmail.com>
2024-03-31 20:53:02 -0300

    Refactor da quest do milho

--- Eldoria/Assets/Scripts/Quests/Farm/CollectCornGoal.cs
+++ Eldoria/Assets/Scripts/Quests/Farm/CollectCornGoal.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class CollectCornGoal : MonoBehaviour
 {
+    public Quest questRelated;
+    public Goal goal;
+    public HudManager hudManager;
+    public GameObject interactionUI;
+    public ParticleSystem cornParticle;
+    public GameObject wagonGoalObject;
+
+    private bool canCollect = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,49 @@ public class CollectCornGoal : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (canCollect && Input.GetKeyDown(KeyCode.F))
+        {
+            CollectCorn();
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (questRelated.enable)
+            {
+                canCollect = true;
+                interactionUI.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canCollect = false;
+            interactionUI.SetActive(false);
+        }
+    }
+
+    private void CollectCorn()
+    {
+        ParticleSystem corn = Instantiate(cornParticle, transform.position, new Quaternion());
+        corn.Play();
+
+        goal.AddAmount();
+
+        string goalDesc = goal.description + "(" + goal.GetAmount() + "/" + goal.GetReqAmount() + ")";
+        hudManager.SetQuestText(goalDesc);
+
+        if (goal.CheckComplete())
+        {
+            //wagonGoalObject.SetActive(true);
+            goal.CompleteGoal();
+            questRelated.CheckForComplete();
+        }
     }
 }
