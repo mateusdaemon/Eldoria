5d44cfab529321fd485f558e2899997f5c449a26
mateusdaemon <mateusjdm@gmail.com>
2024-04-21 00:15:04 -0300

    changing wolf dodge

--- Eldoria/Assets/Scripts/Combat/Enemies/WolfCroc/WolfTakeDamage.cs
+++ Eldoria/Assets/Scripts/Combat/Enemies/WolfCroc/WolfTakeDamage.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using static WolfCrocState;
 
 public class WolfTakeDamage : MonoBehaviour
 {
     public SoundManager sm;
     public GameObject lifeBarUI;
     public ParticleSystem blood;
+    public WolfCrocState wolfState;
 
     private Enemy currEnemy;
+    private bool canDodge = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,15 +32,28 @@ public class WolfTakeDamage : MonoBehaviour
             Instantiate(collision.gameObject.GetComponent<Bullet>().effect, collision.gameObject.transform.position, new Quaternion());
             Destroy(collision.gameObject.gameObject);
 
-            blood.transform.position = collision.collider.ClosestPoint(transform.position);
-            blood.Play();
+            if (canDodge)
+            {
+                wolfState.SetWolfState(WolfCrocState.WolfState.Dodge);
+                canDodge = false;
+                Invoke("ResetDodge", 3.0f);
+            } else
+            {
+                blood.transform.position = collision.collider.ClosestPoint(transform.position);
+                blood.Play();
 
-            currEnemy.TakeDamage(PlayerStats.GetDamage());
+                currEnemy.TakeDamage(PlayerStats.GetDamage());
+            }
         }
     }
 
     private void OnDestroy()
     {
         sm.PlaySfx(sm.sfxWolfDie);
+    }
+
+    private void ResetDodge()
+    {
+        canDodge = true;
     }
 }
